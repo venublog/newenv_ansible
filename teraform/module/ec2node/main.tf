ab12ad1a01c6832ab96ce86a8d04e5c00a20be30
Venugopalan N <39304238+venublog@users.noreply.github.com>
2021-11-29 13:45:46 +0530

    Update main.tf

--- teraform/module/ec2node/main.tf
+++ teraform/module/ec2node/main.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "ec2node" {
   count                = var.ec2node_count
   ami                  = var.application_ami
-  key_name             = var.new_keypair_id
+  key_name             = var.keypair_id
   instance_type        = var.ec2node_instance_type
 
   vpc_security_group_ids = ["var.sg_ec2node_id"]
